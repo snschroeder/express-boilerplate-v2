e7521b2affdb81bd995be88ec1aee6d777ba7747
snschroeder <snschroeder@protonmail.com>
2020-05-04 17:32:41 -0600

    update seed file, sets up postgres and scripts

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -8,8 +8,8 @@ INSERT INTO users ("username", "password", "id")
 VALUES (
   'admin',
   -- 'Password1!' using salt of 12
-'$2a$12$wc5vUYU3XuCnSqaLGVzKu.zzrR.2OTKL977bJBayXpT1bnh9qahcm',
-'7ad87401-dda8-48f0-8ed8-a6bc9756e53c'
-);
+  '$2a$12$wc5vUYU3XuCnSqaLGVzKu.zzrR.2OTKL977bJBayXpT1bnh9qahcm',
+  'f80c78fb-ad06-4e86-9761-d44cafe81ee4'
+ );
 
 COMMIT;
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  DATABASE_URL: process.env.DATABASE_URL || `local hosted db`,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `local hosted test db`,
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
